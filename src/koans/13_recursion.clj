53bbca58735d78d47bcc7a145b4e69db3dffae62
bttf <adnanchowdhury88@gmail.com>
2013-03-25 11:46:10 +0000

    blueberries

--- src/koans/13_recursion.clj
+++ src/koans/13_recursion.clj
@@ -1,20 +1,31 @@
 (defn is-even? [n]
   (if (= n 0)
-    __
-    (___ (is-even? (dec n)))))
+    true
+    (not (is-even? (dec n)))))
 
 (defn is-even-bigint? [n]
   (loop [n   n
          acc true]
     (if (= n 0)
-      __
+     false 
       (recur (dec n) (not acc)))))
 
 (defn recursive-reverse [coll]
-  __)
+  (if (= 1 (count coll))
+    coll
+    (conj (recursive-reverse (drop-last coll)) (last coll))))
+
+; (defn factorial [n]
+;   (if (= 1 n)
+;     n
+;     (* (factorial (dec n)) n)))
 
 (defn factorial [n]
-  __)
+  (loop [n n
+         acc 1]
+    (if (= 0 n)
+      acc 
+      (recur (dec n) (* n acc)))))
 
 (meditations
   "Recursion ends with a base case"
